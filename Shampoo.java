cd56e4b84a80d8adfc52f7f819e359b98c7b9b5c
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:48:08 -0300

    Shampoo.java

---
+++ Shampoo.java
@@ -0,0 +1,21 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Shampoo{
+  private String nome;
+  private int cor;
+
+  public Shampoo(String nome, int cor){
+    this.nome = nome;
+    this.cor = cor;
+  }
+
+  public String getNome(){
+    return this.nome;
+  }
+
+  public int getCor(){
+    return this.cor;
+  }
+
+}
