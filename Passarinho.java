af4fe5a4076168c2f1d3694b965ae55b0b0b7bc3
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:45:40 -0300

    Passarinho.java

---
+++ Passarinho.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Passarinho extends Animal{
+	public Passarinho(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("piu piu");
+	}
+}  
