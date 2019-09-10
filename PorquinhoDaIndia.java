658a350035d5ca1e18f9398dabfd02a830ba84c5
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:47:03 -0300

    PorquinhoDaIndia.java

---
+++ PorquinhoDaIndia.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class PorquinhoDaIndia extends Animal{
+	public PorquinhoDaIndia(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("vi vi");
+	}
+}
