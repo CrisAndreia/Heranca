a118d2596eb7339367db34a6c5ef0faaf25848c8
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:44:43 -0300

    Gato.java

---
+++ Gato.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Gato extends Animal{
+	public Gato(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("miau");
+	}
+}  
