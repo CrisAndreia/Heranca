70b1ba51427b304f45af12dda4913a31e62eb019
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:44:05 -0300

    Galo.java

---
+++ Galo.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Galo extends Animal{
+	public Galo(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("cocoricó");
+  }
+} 
