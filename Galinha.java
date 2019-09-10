b066591c5eaa346f17b5c007ac20f57cc174b705
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:43:23 -0300

    Galinha.java

---
+++ Galinha.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Galinha extends Animal{
+	public Galinha(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("có có");
+	}
+}
