688778da5e54390c396ced946be4758ac81f0d25
CrisAndreia <49501493+CrisAndreia@users.noreply.github.com>
2019-09-10 20:48:48 -0300

    Vaca.java

---
+++ Vaca.java
@@ -0,0 +1,12 @@
+# Heranca
+Projeto de Programação Orientada a Objeto
+
+class Vaca extends Animal{
+	public Vaca(){
+
+	}
+	@Override
+	public void fazerBarulho(){
+		System.out.println("mu mu");
+	}
+} 
